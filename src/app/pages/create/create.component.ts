42f77eaa564421d4b3e5874584577daf6533d275
Samuel-dot-cloud <successam336@gmail.com>
2020-08-02 17:57:58 +0300

    Styling the create page

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -21,9 +21,33 @@ import { GifService } from 'src/app/core/services/gif.service';
  </div>
   `,
   styles: [`
+.gif-container{
+  position: relative;
+}
+
+  .caption{
+    display: block;
+    position: absolute;
+    left: 20px;
+    right: 20px;
+    bottom: 30px;
+    text-align: center;
+    color: #FFF;
+    font-size: 30px;
+    text-transform: uppercase;
+    line-height: 1;
+    word-break: break-all;
+    text-shadow: 1px 1px 3px #000;
+  }
+
   .button {
     display: block;
     width: 100%;
+  }
+
+  img {
+    width: 100%;
+    border-radius: 3px;
   }
   `
   ]
